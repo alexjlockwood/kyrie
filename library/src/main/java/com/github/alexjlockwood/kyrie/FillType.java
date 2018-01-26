18f9fdfcfd0f142c3fb69ac85e3f7c0d6202d9ae
Alex Lockwood <alexjlockwood@gmail.com>
2018-01-26 11:30:55 -0800

    add retention policy to fill type

--- library/src/main/java/com/github/alexjlockwood/kyrie/FillType.java
+++ library/src/main/java/com/github/alexjlockwood/kyrie/FillType.java
@@ -2,6 +2,10 @@ package com.github.alexjlockwood.kyrie;
 
 import android.support.annotation.IntDef;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.SOURCE)
 @IntDef({FillType.NON_ZERO, FillType.EVEN_ODD})
 public @interface FillType {
   int NON_ZERO = 0;
